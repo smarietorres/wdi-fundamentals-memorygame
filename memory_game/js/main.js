378609ee5d56e9c8eb9b4e26a8ca7b4af8ebe064
Stephanie Marie Perez <mariecancode@gmail.com>
2019-09-20 21:12:35 -0400

    function added

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,18 +1,24 @@
 
 var cards = ['Queen', 'Queen', 'King', 'King'];
 var cardsInPlay = [];
-var cardOne = cards[0];
-var cardTwo = cards[2];
-cardsInPlay.push('card0ne');
-cardsInPlay.push('cardTwo');
-console.log('User flipped ' + cardOne);
-console.log('User flipped ' + cardTwo);
 
-if (cardsInPlay.length === 2)
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-	alert("You found a match!");
-} 
-else {
-	alert("Sorry, try again.");
+
+function checkForMatch() {	
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+		console.log("You found a match!");
+	} 
+	else {
+		console.log("Sorry, try again.");
+	}
 }
+
+function flipCard(cardId) {
+	console.log("User flipped a " + cards[cardId] + ".");
+    cardsInPlay.push(cards[cardId]);
+    if (cardsInPlay.length === 2)
+    checkForMatch();
+}
+    flipCard(0);
+    flipCard(1);
+
 
