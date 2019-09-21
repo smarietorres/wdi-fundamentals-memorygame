8cd5e642b730e11c8078d684d330c305c97b16bc
Stephanie Marie Perez <mariecancode@gmail.com>
2019-09-21 18:12:35 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,5 +1,27 @@
 
-var cards = ['Queen', 'Queen', 'King', 'King'];
+var cards = [
+{
+	rank: 'queen',
+	suit: 'hearts',
+	cardImage: 'images/queen-of-hearts.png'
+},
+{
+	rank: 'queen',
+	suit: 'diamonds',
+	cardImage: 'images/queen-of-diamonds.png'
+},
+{
+	rank: 'king',
+	suit: 'hearts',
+	cardImage: 'images/king-of-hearts.png'
+},
+{
+	rank: 'king',
+	suit: 'diamonds',
+	cardImage: 'images/king-of-diamonds.png'
+}
+];
+
 var cardsInPlay = [];
 
 
@@ -10,15 +32,16 @@ function checkForMatch() {
 	else {
 		console.log("Sorry, try again.");
 	}
-}
+};
 
 function flipCard(cardId) {
-	console.log("User flipped a " + cards[cardId] + ".");
-    cardsInPlay.push(cards[cardId]);
-    if (cardsInPlay.length === 2)
-    checkForMatch();
-}
+	console.log("User flipped " + cards[cardId].rank);
+    cardsInPlay.push(cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
+};
     flipCard(0);
     flipCard(1);
 
+checkForMatch();
 
