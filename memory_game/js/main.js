0572e0ec90ceea266db74e746583dd6ee66de730
Stephanie Marie Perez <mariecancode@gmail.com>
2019-09-22 12:35:42 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -37,7 +37,7 @@ function checkForMatch() {
 function flipCard(cardId) {
 	var cardId = this.getAttribute('data-id');
 	cardsInPlay.push(cards[cardId].rank);
-	this.setAttribute('src', cards[cardId.cardImage]);
+	this.setAttribute('src', cards[cardId].cardImage);
     if (cardsInPlay.length === 2) {
     	checkForMatch();
     }
