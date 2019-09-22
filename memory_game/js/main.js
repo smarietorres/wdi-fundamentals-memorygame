a09a99e8950958f6ce95e4c076bcf971d3fcda0c
Stephanie Marie Perez <mariecancode@gmail.com>
2019-09-21 20:32:56 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -27,21 +27,31 @@ var cardsInPlay = [];
 
 function checkForMatch() {	
 		if (cardsInPlay[0] === cardsInPlay[1]) {
-		console.log("You found a match!");
+		alert("You found a match!");
 	} 
 	else {
-		console.log("Sorry, try again.");
+		alert("Sorry, try again.");
 	}
 };
 
 function flipCard(cardId) {
-	console.log("User flipped " + cards[cardId].rank);
-    cardsInPlay.push(cards[cardId].rank);
-	console.log(cards[cardId].cardImage);
-	console.log(cards[cardId].suit);
+	var cardId = this.getAttribute('data-id');
+	cardsInPlay.push(cards[cardId].rank);
+	this.setAttribute('src', cards[cardId.cardImage]);
+    if (cardsInPlay.length === 2) {
+    	checkForMatch();
+    }
 };
-    flipCard(0);
-    flipCard(1);
 
-checkForMatch();
 
+function createBoard() {
+for (var i = 0; i < cards.length; i++) {
+	var cardElement = document.createElement('img');
+	cardElement.setAttribute('src','images/back.png');
+	cardElement.setAttribute('data-id', i);
+	cardElement.addEventListener('click', flipCard);
+	document.getElementById('game-board').appendChild(cardElement);
+}
+};
+
+createBoard()
